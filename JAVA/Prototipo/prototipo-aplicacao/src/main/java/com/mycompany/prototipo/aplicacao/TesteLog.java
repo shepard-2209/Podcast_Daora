d4e242e59abc6c7c1fcf2a5e5abcf5a0bc316e85
shepard-2209 <it-gimenes@hotmail.com>
2019-11-05 13:40:08 -0300

    prototipo logs/ geração de .txt

---
+++ JAVA/Prototipo/prototipo-aplicacao/src/main/java/com/mycompany/prototipo/aplicacao/TesteLog.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prototipo.aplicacao;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class TesteLog {
+
+    public static void main(String[] args) throws IOException {
+        PrototipoLog obj = new PrototipoLog();
+        obj.gerarLog();
+
+    }
+}
